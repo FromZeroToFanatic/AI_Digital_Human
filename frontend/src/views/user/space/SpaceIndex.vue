871e5ea59760916a11a48a0f67c044c279b4ba31
FromZeroToFanatic <18010490531@163.com>
2026-01-28 10:43:27 +0800

    实现路由

---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useRoute, useRouter,} from "vue-router";
+
+const route = useRoute(); // 操作当前url
+const router = useRouter(); // 操作页面相关
+</script>
+
+<template>
+个人空间:{{ route.params.user_id }}
+</template>
+
+<style scoped>
+
+</style>
