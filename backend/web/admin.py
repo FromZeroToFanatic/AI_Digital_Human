97f02c71e3c0dfdb55da0fc9e9079ffaf93eeea3
FromZeroToFanatic <18010490531@163.com>
2026-03-07 09:15:41 +0800

    实现好友页面后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from web.models.user import UserProfile
 from web.models.character import Character
+from web.models.friend import Friend
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',) #逗号不要删，其代表一个列表
 
+
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
